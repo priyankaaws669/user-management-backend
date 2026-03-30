adce0d7e5eb48819ebcdade8af8c44c3fdea2e21
Priyanka Singh <priyankasingh2003111@gmail.com>
2026-03-30 06:56:56 +0530

    fix PATCH API 500 error

--- server.js
+++ server.js
@@ -1,41 +1,70 @@
-const express = require('express');
-const app= express();
+const cors = require("cors");
+const express = require("express");
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let users = [];
+let idCounter = 1;
 
-app.post('/users', (req,res) =>{
-    const newUsers = req.body;
-    users.push(newUsers);
-    res.status(201).json(newUsers);
-})
+//POST
+app.post("/users/post", (req, res) => {
+  const { name, age, email } = req.body;
 
-app.get('/users', (req,res) =>{
-    res.status(200).json(users)
-})
+  const newUsers = {
+    id: idCounter++,
+    name,
+    age,
+    email,
+  };
+  users.push(newUsers);
+  res.status(201).json(newUsers);
+});
 
-app.patch('/users/:id', (req,res) => {
-    const id= req.params.id;
-    
-    if(id >=0 && id< users.length){
-        users[id] = req.body;
-        res.status(200).json(users[id]);
-    }else{res.status(400).send("User not found!")}
-}) 
+//GET
+app.get("/users/get", (req, res) => {
+  res.status(200).json(users);
+});
 
-
-app.delete('/users/:id', (req,res) => {
+//PATCH
+app.patch("/users/patch/:id", (req, res) => {
+  try {
     const id = parseInt(req.params.id);
-    
-    users = users.filter((_,index) => index != id);
 
-    res.status(200).json({
-        users: users, 
-        message:'User deleted!'})
-})
+    const user = users.find((u) => u.id === id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    
+    const {name,age, email} =req.body;
+
+    // update values
+    if (name !== undefined) user.name = name;
+    if (age !== undefined) user.age = age;
+    if (email !== undefined) user.email = email;
+
+    res.json(user);
+  } catch (error) {
+    console.error("PATCH ERROR:", error);
+    res.status(500).json({message: "Server error"})
+  }
+});
+
+//DELETE
+app.delete("/users/delete/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  users = users.filter((user) => user.id != id);
+
+  res.status(200).json({
+    users: users,
+    message: "User deleted!",
+  });
+});
 
 const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
